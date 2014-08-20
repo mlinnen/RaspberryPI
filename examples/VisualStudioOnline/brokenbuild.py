0bcd1915e8f1527a4691967d7af4f1e621c795b0
Mike Linnen <mlinnen@protosystem.net>
2014-08-19 23:24:59 -0400

    more broken build changes

--- examples/VisualStudioOnline/brokenbuild.py
+++ examples/VisualStudioOnline/brokenbuild.py
@@ -3,42 +3,72 @@
 #ptvsd.enable_attach(secret = 'vs2013')
 
 import vsconfig
-import json
-import urllib2, base64
+import json, urllib2, base64, ConfigParser, pygst
 from datetime import datetime
 
 def GetFailedUrl(account, project, dateTime):
 	sdate = "{0}-{1}-{2}%20{3}:{4}:00".format(dateTime.year,dateTime.month,dateTime.day,dateTime.hour,dateTime.minute)
 	return "https://" + account + ".visualstudio.com/DefaultCollection/_apis/build/builds?projectName=" + project + "&status=failed&minFinishTime=" + sdate
 
+def GetLastDateTime():
+	config = ConfigParser.ConfigParser()
+	config.read("data.ini")
+
+	if config.has_section("last"):
+		return datetime.strptime(config.get("last","datetime"),"%Y-%m-%d %H:%M:%S.%f")
+
+	return datetime.now()
+
+def SaveLastDateTime(dateTime):
+	config = ConfigParser.ConfigParser()
+	config.read("data.ini")
+
+	if not config.has_section("last"):
+		# Adds the section and options to the file since they are missing
+		config.add_section("last")
+
+	config.set("last","datetime",dateTime.isoformat(' '))
+
+	# Write out the configuration file
+	with open('data.ini', 'wb') as configfile:
+		config.write(configfile)
+	return
+
+def GetLastBuildData(dateTime):
+	# Setup the request
+	request = urllib2.Request(GetFailedUrl(config.VS_ONLINE_ACCOUNT,config.VS_ONLINE_PROJECT,qDateTime))
+
+	#Do Basic Authentication
+	base64string = base64.encodestring('%s:%s' % (config.VS_ONLINE_USER_NAME, config.VS_ONLINE_PASSWORD)).replace('\n', '')
+	request.add_header("Authorization", "Basic %s" % base64string)   
+
+	# Get the response
+	result = urllib2.urlopen(request)
+
+	data = json.loads(result.read())
+	#with open('data.txt', 'w') as outfile:
+	#  json.dump(data, outfile)
+	return data
+
+
 # Read connection configuration from file
 config = vsconfig.VSOnlineConfig()
 config.read()
 
-api_url = "https://" + config.VS_ONLINE_ACCOUNT + ".visualstudio.com/DefaultCollection/_apis/"
-builds_url = api_url + "build/builds"
-projectBrokenBuilds_url = builds_url + "?projectName=" + config.VS_ONLINE_PROJECT + "&status=failed&minFinishTime=08-19-2014%2020:05:00"
+qDateTime = GetLastDateTime()
 
-qDateTime = datetime(2014,8,19)
-# Setup the request
-request = urllib2.Request(GetFailedUrl(config.VS_ONLINE_ACCOUNT,config.VS_ONLINE_PROJECT,qDateTime))
+data = GetLastBuildData(qDateTime)
 
-#Do Basic Authentication
-base64string = base64.encodestring('%s:%s' % (config.VS_ONLINE_USER_NAME, config.VS_ONLINE_PASSWORD)).replace('\n', '')
-request.add_header("Authorization", "Basic %s" % base64string)   
+dateTimeNow = datetime.utcnow()
 
-# Get the response
-result = urllib2.urlopen(request)
-
-data = json.loads(result.read())
-#with open('data.txt', 'w') as outfile:
-#  json.dump(data, outfile)
+SaveLastDateTime(dateTimeNow)
 
 count = data["count"]
 if count==0:
 	print "no broken builds found"
 else:
-	i = count - 1
+	print count
+	i = 0
 	displayName = data["value"][i]["requests"][0]["requestedFor"]["displayName"]
 	status = data["value"][i]["status"]
 	buildNumber = data["value"][i]["buildNumber"]
